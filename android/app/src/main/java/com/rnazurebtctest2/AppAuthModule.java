0e4ef46ec46c4aada39ea58a5fc0e7a60a54917a
Ian Ellison-Taylor <4thjuly@ellison-taylor.com>
2017-08-19 21:56:05 -0700

    Debugging enabled

--- android/app/src/main/java/com/rnazurebtctest2/AppAuthModule.java
+++ android/app/src/main/java/com/rnazurebtctest2/AppAuthModule.java
@@ -6,7 +6,10 @@ import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
+//import net.openid.appauth.*;
 import java.util.Map;
+
+import android.net.Uri;
 import android.widget.Toast;
 
 public class AppAuthModule extends ReactContextBaseJavaModule {
@@ -22,11 +25,13 @@ public class AppAuthModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void configureService() {
-        Toast.makeText(getReactApplicationContext(), "Testing Native Method", Toast.LENGTH_LONG).show();
+        String mAuthEndpoint = "https://login.microsoftonline.com/IanETB2CTenant.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_SUSI";
+        String mTokenEndpoint = "https://login.microsoftonline.com/IanETB2CTenant.onmicrosoft.com/oauth2/v2.0/token?p=B2C_1_SUSI";
+        String clientId = "62386987-856b-4e6e-89db-59eef6d603b6";
+        Uri redirectUri = Uri.parse("com.onmicrosoft.ianetb2ctenant.rnazureb2ctest2://oauth/redirect");
 
-//        AuthorizationServiceConfiguration serviceConfiguration = new AuthorizationServiceConfiguration(
-//                Uri.parse("https://accounts.google.com/o/oauth2/v2/auth") /* auth endpoint */,
-//                Uri.parse("https://www.googleapis.com/oauth2/v4/token") /* token endpoint */
-//        );
+        Toast.makeText(getReactApplicationContext(), "Testing Native Method", Toast.LENGTH_LONG).show();
+//        AuthorizationServiceConfiguration config = new AuthorizationServiceConfiguration(Uri.parse(mAuthEndpoint), Uri.parse(mTokenEndpoint), null);
+//        AuthorizationRequest req = new AuthorizationRequest.Builder(config, clientId, ResponseTypeValues.CODE, redirectUri).build();
     }
 }
