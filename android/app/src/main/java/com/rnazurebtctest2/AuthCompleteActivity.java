dff60c2e3075b7e5b7d2f6ebf370731eeb715fff
Ian Ellison-Taylor <4thjuly@ellison-taylor.com>
2017-08-31 17:43:52 +0700

    Fixed redirect, works!

--- android/app/src/main/java/com/rnazurebtctest2/AuthCompleteActivity.java
+++ android/app/src/main/java/com/rnazurebtctest2/AuthCompleteActivity.java
@@ -3,11 +3,25 @@ package com.rnazurebtctest2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import net.openid.appauth.*;
+
+import android.widget.Toast;
+
 public class AuthCompleteActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth_complete);
+
+        AuthorizationResponse resp = AuthorizationResponse.fromIntent(getIntent());
+        AuthorizationException ex = AuthorizationException.fromIntent(getIntent());
+        if (resp != null) {
+            Toast.makeText(this, "Auth Succeeded", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Auth Failed", Toast.LENGTH_LONG).show();
+        }
     }
+
+
 }
