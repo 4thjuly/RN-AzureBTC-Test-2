75eb78c1f1aca0d182b98ff3dc5b99d80de2a5f2
Ian Ellison-Taylor <4thjuly@ellison-taylor.com>
2017-08-31 10:59:21 +0700

    Now call Auth api from new Activity
    Call seems to work but url is invalid

--- android/app/src/main/java/com/rnazurebtctest2/AppAuthModule.java
+++ android/app/src/main/java/com/rnazurebtctest2/AppAuthModule.java
@@ -1,19 +1,11 @@
 package com.rnazurebtctest2;
 
-import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
-import net.openid.appauth.*;
-import java.util.Map;
-
-import android.app.Activity;
-import android.app.PendingIntent;
 import android.content.Intent;
-import android.net.Uri;
-import android.widget.Toast;
 
 public class AppAuthModule extends ReactContextBaseJavaModule {
 
@@ -32,24 +24,5 @@ public class AppAuthModule extends ReactContextBaseJavaModule {
         Intent authStartIntent = new Intent(context, AuthStartActivity.class);
         authStartIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(authStartIntent);
-
-
-
-//        String authEndpoint = "https://login.microsoftonline.com/IanETB2CTenant.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_SUSI";
-//        String tokenEndpoint = "https://login.microsoftonline.com/IanETB2CTenant.onmicrosoft.com/oauth2/v2.0/token?p=B2C_1_SUSI";
-//        String clientId = "62386987-856b-4e6e-89db-59eef6d603b6";
-//        Uri redirectUri = Uri.parse("com.onmicrosoft.ianetb2ctenant.rnazureb2ctest2://oauth/redirect");
-//        ReactContext context = getReactApplicationContext();
-//        Activity activity = getCurrentActivity();
-//
-//        Toast.makeText(context, "Testing Native Method", Toast.LENGTH_LONG).show();
-//        AuthorizationServiceConfiguration config = new AuthorizationServiceConfiguration(Uri.parse(authEndpoint), Uri.parse(tokenEndpoint));
-//        AuthorizationRequest req = new AuthorizationRequest.Builder(config, clientId, "code", redirectUri).build();
-//        AuthorizationService authService = new AuthorizationService(context);
-//
-//        Intent completeIntent = new Intent(context, AuthCompleteActivity.class);
-//        completeIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//
-//        authService.performAuthorizationRequest(req, PendingIntent.getActivity(context, 0, completeIntent, 0));
     }
 }
--- android/app/src/main/java/com/rnazurebtctest2/AuthStartActivity.java
+++ android/app/src/main/java/com/rnazurebtctest2/AuthStartActivity.java
@@ -6,6 +6,13 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactApplicationContext;
+
+import net.openid.appauth.*;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.net.Uri;
 
 public class AuthStartActivity extends AppCompatActivity {
 
@@ -18,8 +25,19 @@ public class AuthStartActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        Context context = getApplicationContext();
-        Toast.makeText(context, "Testing Native Method", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Testing Native Method", Toast.LENGTH_LONG).show();
+
+        String authEndpoint = "https://login.microsoftonline.com/IanETB2CTenant.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_SUSI";
+        String tokenEndpoint = "https://login.microsoftonline.com/IanETB2CTenant.onmicrosoft.com/oauth2/v2.0/token?p=B2C_1_SUSI";
+        String clientId = "62386987-856b-4e6e-89db-59eef6d603b6";
+        Uri redirectUri = Uri.parse("com.onmicrosoft.ianetb2ctenant.rnazureb2ctest2://oauth/redirect");
+
+        AuthorizationServiceConfiguration config = new AuthorizationServiceConfiguration(Uri.parse(authEndpoint), Uri.parse(tokenEndpoint));
+        AuthorizationRequest req = new AuthorizationRequest.Builder(config, clientId, "code", redirectUri).build();
+        AuthorizationService authService = new AuthorizationService(this);
+
+        Intent completeIntent = new Intent(this, AuthCompleteActivity.class);
+        authService.performAuthorizationRequest(req, PendingIntent.getActivity(this, 0, completeIntent, 0));
     }
 
 }
