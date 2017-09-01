1dcf980ba6d385eb7cd0bbddf608ac8167c56568
Ian Ellison-Taylor <4thjuly@ellison-taylor.com>
2017-09-01 11:51:20 +0700

    Enabled MSClient, calls "Test" method
    Disable all auth to just get it working

--- android/app/src/main/java/com/rnazurebtctest2/AuthCompleteActivity.java
+++ android/app/src/main/java/com/rnazurebtctest2/AuthCompleteActivity.java
@@ -4,9 +4,13 @@ import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import net.openid.appauth.*;
+//import net.openid.appauth.*;
 
 import android.widget.Toast;
+import com.microsoft.windowsazure.mobileservices.*;
+//import com.google.common.util.concurrent.*;
+//import com.google.gson.*;
+import java.net.MalformedURLException;
 
 public class AuthCompleteActivity extends AppCompatActivity {
 
@@ -15,28 +19,46 @@ public class AuthCompleteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth_complete);
 
-        AuthorizationResponse resp = AuthorizationResponse.fromIntent(getIntent());
-        AuthorizationException ex = AuthorizationException.fromIntent(getIntent());
-        if (resp != null) {
-            final Activity activity = this;
-            Toast.makeText(this, "Auth Succeeded", Toast.LENGTH_LONG).show();
-            MainApplication app = (MainApplication) getApplication();
-            AuthorizationService authService = app.authService;
-            authService.performTokenRequest(
-                    resp.createTokenExchangeRequest(),
-                    new AuthorizationService.TokenResponseCallback() {
-                        @Override public void onTokenRequestCompleted(TokenResponse resp, AuthorizationException ex) {
-                            if (resp != null) {
-                                Toast.makeText(activity, "Token request succeeded", Toast.LENGTH_LONG).show();
-                            } else {
-                                Toast.makeText(activity, "Token request failed", Toast.LENGTH_LONG).show();
-                            }
-                        }
-                    });
-
-        } else {
-            Toast.makeText(this, "Auth Failed", Toast.LENGTH_LONG).show();
-        }
+//        AuthorizationResponse resp = AuthorizationResponse.fromIntent(getIntent());
+//        AuthorizationException ex = AuthorizationException.fromIntent(getIntent());
+//        if (resp != null) {
+//            final Activity activity = this;
+//            Toast.makeText(this, "Auth Succeeded", Toast.LENGTH_LONG).show();
+//            MainApplication app = (MainApplication) getApplication();
+//            AuthorizationService authService = app.authService;
+//            authService.performTokenRequest(
+//                    resp.createTokenExchangeRequest(),
+//                    new AuthorizationService.TokenResponseCallback() {
+//                        @Override public void onTokenRequestCompleted(TokenResponse resp, AuthorizationException ex) {
+//                            if (resp != null) {
+//                                Toast.makeText(activity, "Token request succeeded", Toast.LENGTH_LONG).show();
+//                                try {
+////                                    MobileServiceClient client = new MobileServiceClient("https://api-app-test-1.azurewebsites.net", activity);
+////                                    client.invokeApi("Test");
+//
+////                                    ListenableFuture<com.google.gson.JsonElement> result = client.invokeApi("Test");
+////                                    Futures.addCallback(result, new FutureCallback<Object>() {
+////                                        @Override
+////                                        public void onFailure(Throwable exc) {
+////                                        }
+////
+////                                        @Override
+////                                        public void onSuccess(JsonElement result) {
+////
+////                                        }
+////                                    });
+//                                } catch (Exception e) {
+//                                    e.printStackTrace();
+//                                }
+//                            } else {
+//                                Toast.makeText(activity, "Token request failed", Toast.LENGTH_LONG).show();
+//                            }
+//                        }
+//                    });
+//
+//        } else {
+//            Toast.makeText(this, "Auth Failed", Toast.LENGTH_LONG).show();
+//        }
     }
 
 
--- android/app/src/main/java/com/rnazurebtctest2/AuthStartActivity.java
+++ android/app/src/main/java/com/rnazurebtctest2/AuthStartActivity.java
@@ -7,12 +7,17 @@ import android.widget.Toast;
 
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
 
-import net.openid.appauth.*;
+//import net.openid.appauth.*;
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.net.Uri;
+import com.google.common.util.concurrent.*;
+import com.google.gson.*;
+
+import java.util.Collections;
 
 public class AuthStartActivity extends AppCompatActivity {
 
@@ -25,22 +30,33 @@ public class AuthStartActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        Toast.makeText(this, "Testing Native Method", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Testing Native Method (2)", Toast.LENGTH_LONG).show();
+        final Activity activity = this;
 
-        String authEndpoint = "https://login.microsoftonline.com/IanETB2CTenant.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_SUSI";
-        String tokenEndpoint = "https://login.microsoftonline.com/IanETB2CTenant.onmicrosoft.com/oauth2/v2.0/token?p=B2C_1_SUSI";
-        String clientId = "62386987-856b-4e6e-89db-59eef6d603b6";
-        String scope = "openid profile";
-        Uri redirectUri = Uri.parse("com.onmicrosoft.ianetb2ctenant.rnazureb2ctest2://oauth/redirect");
+        try {
+            MobileServiceClient client = new MobileServiceClient("https://api-app-test-1.azurewebsites.net", activity);
+            ListenableFuture<JsonElement> test = client.invokeApi("Test", "GET", Collections.EMPTY_LIST);
+            Toast.makeText(this, "Invoked Test", Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        AuthorizationServiceConfiguration config = new AuthorizationServiceConfiguration(Uri.parse(authEndpoint), Uri.parse(tokenEndpoint));
-        AuthorizationRequest req = new AuthorizationRequest.Builder(config, clientId, "code", redirectUri).setScope(scope).build();
-        AuthorizationService authService = new AuthorizationService(this);
-        MainApplication app = (MainApplication) getApplication();
-        app.authService = authService;
 
-        Intent completeIntent = new Intent(this, AuthCompleteActivity.class);
-        authService.performAuthorizationRequest(req, PendingIntent.getActivity(this, 0, completeIntent, 0));
+
+//        String authEndpoint = "https://login.microsoftonline.com/IanETB2CTenant.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_SUSI";
+//        String tokenEndpoint = "https://login.microsoftonline.com/IanETB2CTenant.onmicrosoft.com/oauth2/v2.0/token?p=B2C_1_SUSI";
+//        String clientId = "62386987-856b-4e6e-89db-59eef6d603b6";
+//        String scope = "openid profile";
+//        Uri redirectUri = Uri.parse("com.onmicrosoft.ianetb2ctenant.rnazureb2ctest2://oauth/redirect");
+//
+//        AuthorizationServiceConfiguration config = new AuthorizationServiceConfiguration(Uri.parse(authEndpoint), Uri.parse(tokenEndpoint));
+//        AuthorizationRequest req = new AuthorizationRequest.Builder(config, clientId, "code", redirectUri).setScope(scope).build();
+//        AuthorizationService authService = new AuthorizationService(this);
+//        MainApplication app = (MainApplication) getApplication();
+//        app.authService = authService;
+//
+//        Intent completeIntent = new Intent(this, AuthCompleteActivity.class);
+//        authService.performAuthorizationRequest(req, PendingIntent.getActivity(this, 0, completeIntent, 0));
     }
 
 }
