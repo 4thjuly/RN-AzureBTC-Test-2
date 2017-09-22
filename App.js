bfa66e340784d22eb2afbb80e9db69bb591fbcda
Ian Ellison-Taylor <4thjuly@ellison-taylor.com>
2017-09-22 15:54:22 -0700

    Start on QR Scanner

--- App.js
+++ App.js
@@ -2,27 +2,10 @@ import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import AppAuthAndroid from './AppAuthAndroid';
 import QRCode from 'react-native-qrcode';
+import { StackNavigator } from 'react-navigation';
+import MainUI from './MainUI';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={{margin:5}}>Sample app with native module - v0.03</Text>
-        <View style={{flexDirection:'row', margin:5}}>
-        <Button style={{margin:5}} title=" Logon " onPress={ () => { AppAuthAndroid.login() }} /> 
-        <Button style={{margin:5}} title=" Logoff " onPress={ () => { AppAuthAndroid.logout() }} /> 
-        </View>
-        <QRCode value='This is a test' size={200} bgColor='black' fgColor='white'/>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const App = StackNavigator({
+  MainUI: { screen: MainUI }
 });
+
