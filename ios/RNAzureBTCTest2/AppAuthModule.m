8844c1c72e1c94cf1c8957bfabe22daa668f22c6
4thjuly <ian@iet-mbp.local>
2017-11-17 13:32:04 -0800

    Cleanup

--- ios/RNAzureBTCTest2/AppAuthModule.m
+++ ios/RNAzureBTCTest2/AppAuthModule.m
@@ -29,10 +29,7 @@
     
     OIDAuthorizationRequest *request = [[OIDAuthorizationRequest alloc] initWithConfiguration:configuration
         clientId:clientId
-//        scopes:@[@"openid", @"profile", @"offline_access"]
         scopes:@[@"openid", @"profile", @"offline_access", @"62386987-856b-4e6e-89db-59eef6d603b6"]
-//        scopes:@[@"62386987-856b-4e6e-89db-59eef6d603b6"]
-//        scopes:@[@"62386987-856b-4e6e-89db-59eef6d603b6", @	"offline_access"]
         redirectURL:redirectURI
         responseType:OIDResponseTypeCode
         additionalParameters:nil];
@@ -58,23 +55,5 @@
         }
     }];
 }
-
-
-//String authEndpoint = "https://login.microsoftonline.com/IanETB2CTenant.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_SUSI";
-//String tokenEndpoint = "https://login.microsoftonline.com/IanETB2CTenant.onmicrosoft.com/oauth2/v2.0/token?p=B2C_1_SUSI";
-//String clientId = "62386987-856b-4e6e-89db-59eef6d603b6";
-//String scope = "openid profile offline_access 62386987-856b-4e6e-89db-59eef6d603b6";
-//Uri redirectUri = Uri.parse("com.onmicrosoft.ianetb2ctenant.rnazureb2ctest2://oauth/redirect");
-//
-//AuthorizationServiceConfiguration config = new AuthorizationServiceConfiguration(Uri.parse(authEndpoint), Uri.parse(tokenEndpoint));
-//AuthorizationRequest req = new AuthorizationRequest.Builder(config, clientId, "code", redirectUri).setScope(scope).build();
-//AuthorizationService authService = new AuthorizationService(this);
-//MainApplication app = (MainApplication) getApplication();
-//app.authService = authService;
-//app.authConfig = config;
-//
-//Intent completeIntent = new Intent(this, AuthCompleteActivity.class);
-//authService.performAuthorizationRequest(req, PendingIntent.getActivity(this, 0, completeIntent, 0));
-
 
 @end
